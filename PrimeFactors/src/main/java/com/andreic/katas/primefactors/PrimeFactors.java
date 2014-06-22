e2cd62875c3d7b2cb453d42c4b28e81b17f52870
Andrei Catinean <andrei@novoda.com>
2014-06-22 20:27:22 +0100

    Adds first implementation of PrimeFactors. - GREEN

---
+++ PrimeFactors/src/main/java/com/andreic/katas/primefactors/PrimeFactors.java
@@ -0,0 +1,14 @@
+package com.andreic.katas.primefactors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andreicatinean on 22/06/2014.
+ */
+public class PrimeFactors {
+    public static List<Integer> generate(int number) {
+        List<Integer> factors = new ArrayList<Integer>();
+        return factors;
+    }
+}
