894c905cd8cdffa37b00e9aafbfcb58f6a96662a
Andrei Catinean <andrei@novoda.com>
2014-06-22 21:45:12 +0100

    Extracts methods - REFACTOR

--- PrimeFactors/src/main/java/com/andreic/katas/primefactors/PrimeFactors.java
+++ PrimeFactors/src/main/java/com/andreic/katas/primefactors/PrimeFactors.java
@@ -9,20 +9,42 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> generate(int number) {
         List<Integer> factors = new ArrayList<Integer>();
+
         int factor = 2;
         while (factor <= number) {
-            if ((number % factor == 0) && isPrime(factor)) {
+            if (isProductWithLastFactorOverflowsFor(number, factors, factor)) {
+                break;
+            }
+
+            if (isPrimeFactorFor(number, factor)) {
                 factors.add(factor);
             }
+
             factor++;
         }
 
         return factors;
     }
 
-    private static boolean isPrime(int factor) {
-        for (int i = 2; i <= Math.sqrt(factor); i++){
-            if (factor % i == 0){
+    private static boolean isProductWithLastFactorOverflowsFor(int number, List<Integer> factors, int factor) {
+        return (factors.size() > 0) && (factor * lastItemOf(factors) > number);
+    }
+
+    private static Integer lastItemOf(List<Integer> list) {
+        return list.get(list.size() - 1);
+    }
+
+    private static boolean isPrimeFactorFor(int number, int factor) {
+        return isFactorFor(number, factor) && isPrime(factor);
+    }
+
+    private static boolean isFactorFor(int number, int factor) {
+        return (number % factor == 0);
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (isFactorFor(number, i)) {
                 return false;
             }
         }
