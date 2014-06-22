06518bab619735dd12cf392d77cb343d411cb0cc
Andrei Catinean <andrei@novoda.com>
2014-06-22 17:56:36 +0100

    Refactors test to use new typed object.

--- RomanNumerals/src/test/java/com/andreic/katas/romannumerals/RomanNumeralConverterTest.java
+++ RomanNumerals/src/test/java/com/andreic/katas/romannumerals/RomanNumeralConverterTest.java
@@ -10,32 +10,32 @@ import static org.fest.assertions.Assertions.assertThat;
  */
 public class RomanNumeralConverterTest {
 
-    public static final String ZERO = "0";
-    public static final String ROMAN_1 = "I";
-    public static final String ROMAN_2 = "II";
-    public static final String ROMAN_3 = "III";
-    public static final String ROMAN_4 = "IV";
-    public static final String ROMAN_5 = "V";
-    public static final String ROMAN_6 = "VI";
-    public static final String ROMAN_7 = "VII";
-    public static final String ROMAN_8 = "VIII";
-    public static final String ROMAN_9 = "IX";
-    public static final String ROMAN_10 = "X";
-    public static final String ROMAN_11 = "XI";
-    public static final String ROMAN_12 = "XII";
-    public static final String ROMAN_13 = "XIII";
-    public static final String ROMAN_14 = "XIV";
-    public static final String ROMAN_15 = "XV";
-    public static final String ROMAN_16 = "XVI";
-    public static final String ROMAN_17 = "XVII";
-    public static final String ROMAN_18 = "XVIII";
-    public static final String ROMAN_19 = "XIX";
-    public static final String ROMAN_24 = "XXIV";
-    public static final String ROMAN_44 = "XLIV";
-    public static final String ROMAN_904 = "CMIV";
-    public static final String ROMAN_1977 = "MCMLXXVII";
-    public static final String ROMAN_1999 = "MCMXCIX";
-    public static final String ROMAN_2014 = "MMXIV";
+    public static final RomanSymbol ZERO = new RomanSymbol(0, "0");
+    public static final RomanSymbol ROMAN_1 = new RomanSymbol(1, "I");
+    public static final RomanSymbol ROMAN_2 = new RomanSymbol(2, "II");
+    public static final RomanSymbol ROMAN_3 = new RomanSymbol(3, "III");
+    public static final RomanSymbol ROMAN_4 = new RomanSymbol(4, "IV");
+    public static final RomanSymbol ROMAN_5 = new RomanSymbol(5, "V");
+    public static final RomanSymbol ROMAN_6 = new RomanSymbol(6, "VI");
+    public static final RomanSymbol ROMAN_7 = new RomanSymbol(7, "VII");
+    public static final RomanSymbol ROMAN_8 = new RomanSymbol(8, "VIII");
+    public static final RomanSymbol ROMAN_9 = new RomanSymbol(9, "IX");
+    public static final RomanSymbol ROMAN_10 = new RomanSymbol(10, "X");
+    public static final RomanSymbol ROMAN_11 = new RomanSymbol(11, "XI");
+    public static final RomanSymbol ROMAN_12 = new RomanSymbol(12, "XII");
+    public static final RomanSymbol ROMAN_13 = new RomanSymbol(13, "XIII");
+    public static final RomanSymbol ROMAN_14 = new RomanSymbol(14, "XIV");
+    public static final RomanSymbol ROMAN_15 = new RomanSymbol(15, "XV");
+    public static final RomanSymbol ROMAN_16 = new RomanSymbol(16, "XVI");
+    public static final RomanSymbol ROMAN_17 = new RomanSymbol(17, "XVII");
+    public static final RomanSymbol ROMAN_18 = new RomanSymbol(18, "XVIII");
+    public static final RomanSymbol ROMAN_19 = new RomanSymbol(19, "XIX");
+    public static final RomanSymbol ROMAN_24 = new RomanSymbol(24, "XXIV");
+    public static final RomanSymbol ROMAN_44 = new RomanSymbol(44, "XLIV");
+    public static final RomanSymbol ROMAN_904 = new RomanSymbol(904, "CMIV");
+    public static final RomanSymbol ROMAN_1977 = new RomanSymbol(1977, "MCMLXXVII");
+    public static final RomanSymbol ROMAN_1999 = new RomanSymbol(1999, "MCMXCIX");
+    public static final RomanSymbol ROMAN_2014 = new RomanSymbol(2014, "MMXIV");
 
     private RomanNumeralConverter romanNumeralConverter;
 
@@ -108,7 +108,7 @@ public class RomanNumeralConverterTest {
     }
 
     @Test
-     public void test19ReturnsCorrectRoman() {
+    public void test19ReturnsCorrectRoman() {
         assertThat(romanNumeralConverter.toRoman(19)).isEqualTo(ROMAN_19);
     }
 
